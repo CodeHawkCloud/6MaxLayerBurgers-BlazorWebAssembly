2e96de3f4b72624552aebaa9a9f98393225b55e1
Nimician Shanil Rajintha Corera <20202857@student.curtin.edu.au>
2021-11-23 00:46:40 +0800

    Preliminary View All burgers completed

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using _6MaxLayerBurgers_BlazorWebAssembly.Client.Services;
 
 namespace _6MaxLayerBurgers_BlazorWebAssembly.Client
 {
@@ -18,6 +19,8 @@ namespace _6MaxLayerBurgers_BlazorWebAssembly.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IBurgerService, BurgerService>();
 
             await builder.Build().RunAsync();
         }
